aea2e609ab59557302497b26df5e8c84f2c87f4d
Domenick Poster <dposter@mix.wvu.edu>
2017-10-02 13:52:11 -0400

    Changing checking account docs

--- src/exercise4/CheckingAccount.java
+++ src/exercise4/CheckingAccount.java
@@ -5,24 +5,31 @@
  * Date:
  * 
  * Purpose: Abstract and concrete classes. Inheritance. Static and non-static members.
- * 
  */
 
 package exercise4;
 
 /*******************************************************************************
-
 This class is a subclass of the class Account.  It is intended to represent
-checking accounts that have a minimum balance.  It inherits all of the instance
-fields of class account but can not directly access them.  It also inherits the
-operations provided for class account.
+checking accounts. It inherits all of the instance fields of class account
+but can not directly access them.  It also inherits the operations provided
+for class account.
 
 Class Variables:
 		penalty:  Amount of penalty to be applied to the account
 			should it fall below it's minimum balance.
 
 Instance Variables:
-		minBalance:  Minimum balance required for the account.
-		    minBalance can be negative to allow overdraft.
+		minBalance:  Minimum balance required for the account. 
+			minBalance can be a negative amount.
 *******************************************************************************/
 //TODO: Declare the class here
+
+
+	/*******************************************************************************
+	Constructor for class CheckingAccount. It creates a new instance of the account.
+	To fill the private instance variables inherited from class Account, it invokes
+	the constructor for class account through the call to Account's constructor via 
+	the "super" identifier.  Super refers to a class's superclass.
+	*******************************************************************************/
+
